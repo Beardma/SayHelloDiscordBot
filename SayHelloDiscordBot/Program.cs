7dc1bbbe7b522fc4d916b39440a5618a16df26b5
Marshall Beard <Marshall Beard@MARSHALL-FRAMEW>
2022-02-26 13:13:00 -0500

    Add project files.

---
+++ SayHelloDiscordBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Configuration;
+using NLog;
+public class Program
+{
+    private Discord.WebSocket.DiscordSocketClient _client;
+    private Logger _log;
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        Logger log = LogManager.GetCurrentClassLogger();
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = "token";
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+}
